323df2bd0445d58d8252181b7a177cb65c0a1eef
cody1991 <476490767@qq.com>
2015-04-25 10:36:22 +0800

    change config

--- main/gulpfile.js
+++ main/gulpfile.js
@@ -1,7 +1,4 @@
-// 引入 gulp
 var gulp = require('gulp');
-
-// 引入组件
 var jshint = require('gulp-jshint');
 var less = require('gulp-less');
 var concat = require('gulp-concat');
@@ -9,15 +6,21 @@ var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 var minifycss = require('gulp-minify-css');
 
-// 检查脚本
 gulp.task('lint', function() {
     gulp.src('./js/*.js')
         .pipe(jshint())
         .pipe(jshint.reporter('default'));
 });
 
+gulp.task('scripts', function() {
+    gulp.src('./js/*.js')
+        .pipe(concat('all.js'))
+        .pipe(gulp.dest('./dist'))
+        .pipe(rename('all.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist'));
+});
 
-// 编译less
 gulp.task('less', function() {
     gulp.src('./less/*.less')
         .pipe(less({
@@ -37,22 +40,17 @@ gulp.task('min-styles', ['less'], function() {
         .pipe(gulp.dest('./dist'));
 });
 
-// 合并，压缩文件
-gulp.task('scripts', function() {
-    gulp.src('./js/*.js')
-        .pipe(concat('all.js'))
-        .pipe(gulp.dest('./dist'))
-        .pipe(rename('all.min.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest('./dist'));
-});
-
-// 默认任务
 gulp.task('default', function() {
     gulp.run('lint', 'scripts', 'less', 'min-styles');
 
     // 监听文件变化
     gulp.watch('./js/*.js', function() {
-        gulp.run('lint', 'scripts', 'less', 'min-styles');
+        gulp.run('lint', 'scripts');
     });
+    gulp.watch('./less/*.less', function() {
+        gulp.run('less');
+    })
+    gulp.watch('./css/*.css', function() {
+        gulp.run('min-styles');
+    })
 });
