bde874a6778894aebcb67904182b184d4e215425
cody1991 <476490767@qq.com>
2015-07-01 17:38:13 +0800

    add gulp-connect and the function of create all.js and all.css :dog:

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,19 @@ var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 var minifycss = require('gulp-minify-css');
 var lessPluginAutoPrefix = require('less-plugin-autoprefix');
+var connect = require('gulp-connect');
+
+gulp.task('connect', function() {
+    connect.server({
+        root: './',
+        livereload: true
+    });
+});
+
+gulp.task('reload', function() {
+    gulp.src('./*')
+        .pipe(connect.reload());
+})
 
 var autoprefix = new lessPluginAutoPrefix({
     browsers: [
@@ -22,39 +35,64 @@ var autoprefix = new lessPluginAutoPrefix({
 });
 
 gulp.task('lint', function() {
-    gulp.src('./js/common.js').pipe(jshint()).pipe(jshint.reporter('default'));
+    gulp.src('./js/common.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
 });
 
 gulp.task('scripts', function() {
-    gulp.src(['./js/jquery-2.1.4.min.js', './js/common.js']).pipe(concat('all.js')).pipe(gulp.dest('./dist')).pipe(rename('all.min.js')).pipe(uglify()).pipe(gulp.dest('./online/dist')).pipe(gulp.dest('./dist/'));
+    gulp.src(['./js/jquery-2.1.4.min.js', './js/common.js'])
+        .pipe(concat('all.js'))
+        .pipe(gulp.dest('./dist'))
+        .pipe(rename('all.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('./online/dist'))
+        .pipe(gulp.dest('./dist/'));
 });
 
 gulp.task('less', function() {
-    gulp.src('./less/*.less').pipe(less({
-        compress: false,
-        plugins: [autoprefix]
-    })).pipe(gulp.dest('./css'));
+    gulp.src('./less/*.less')
+        .pipe(less({
+            compress: false,
+            plugins: [autoprefix]
+        }))
+        .pipe(gulp.dest('./css'));
 });
 
 gulp.task('min-styles', ['less'], function() {
-    gulp.src(['./css/normalize.css', './css/style.css']).pipe(concat('all.css')).pipe(gulp.dest('./dist')).pipe(rename('all.min.css')).pipe(minifycss()).pipe(gulp.dest('./online/dist')).pipe(gulp.dest('./dist/'));
+    gulp.src(['./css/normalize.css', './css/style.css'])
+        .pipe(concat('all.css'))
+        .pipe(gulp.dest('./dist'))
+        .pipe(rename('all.min.css'))
+        .pipe(minifycss())
+        .pipe(gulp.dest('./online/dist'))
+        .pipe(gulp.dest('./dist/'));
 });
 
 gulp.task('online', function() {
-    gulp.src('./images/*').pipe(gulp.dest('./online/images/'));
-    gulp.src('./*.html').pipe(gulp.dest('./online/'));
-    gulp.src('./lib/*').pipe(gulp.dest('./online/lib/'));
+    gulp.src('./images/*')
+        .pipe(gulp.dest('./online/images/'));
+
+    gulp.src('./*.html')
+        .pipe(gulp.dest('./online/'));
+
+    gulp.src('./lib/*')
+        .pipe(gulp.dest('./online/lib/'));
 })
 
 gulp.task('default', function() {
-    gulp.run('lint', 'scripts', 'less', 'min-styles', 'online');
+    gulp.run('connect', 'lint', 'scripts', 'less', 'min-styles', 'online');
+
     gulp.watch('./js/*.js', function() {
-        gulp.run('lint', 'scripts');
+        gulp.run('lint', 'scripts', 'reload');
+
     });
+
     gulp.watch('./less/*.less', function() {
-        gulp.run('less', 'min-styles');
+        gulp.run('less', 'min-styles', 'reload');
     });
+
     gulp.watch(['./index.html', './images/*'], function() {
-        gulp.run('online');
+        gulp.run('online', 'reload');
     });
 })
