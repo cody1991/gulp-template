fb75687fdaab260b2668a56b640e3682f1584644
cody1991 <476490767@qq.com>
2015-07-01 11:36:22 +0800

    change config to make more simple

--- gulpfile.js
+++ gulpfile.js
@@ -26,7 +26,7 @@ gulp.task('lint', function() {
 });
 
 gulp.task('scripts', function() {
-    gulp.src(['./js/jquery-2.1.4.min.js','./js/common.js']).pipe(concat('all.js')).pipe(gulp.dest('./dist')).pipe(rename('all.min.js')).pipe(uglify()).pipe(gulp.dest('./online/dist')).pipe(gulp.dest('./dist/'));
+    gulp.src(['./js/jquery-2.1.4.min.js', './js/common.js']).pipe(concat('all.js')).pipe(gulp.dest('./dist')).pipe(rename('all.min.js')).pipe(uglify()).pipe(gulp.dest('./online/dist')).pipe(gulp.dest('./dist/'));
 });
 
 gulp.task('less', function() {
@@ -37,24 +37,24 @@ gulp.task('less', function() {
 });
 
 gulp.task('min-styles', ['less'], function() {
-    gulp.src('./css/*.css').pipe(concat('all.css')).pipe(gulp.dest('./dist')).pipe(rename('all.min.css')).pipe(minifycss()).pipe(gulp.dest('./online/dist')).pipe(gulp.dest('./dist/'));
+    gulp.src(['./css/normalize.css', './css/style.css']).pipe(concat('all.css')).pipe(gulp.dest('./dist')).pipe(rename('all.min.css')).pipe(minifycss()).pipe(gulp.dest('./online/dist')).pipe(gulp.dest('./dist/'));
 });
 
-gulp.task('online',function(){
+gulp.task('online', function() {
     gulp.src('./images/*').pipe(gulp.dest('./online/images/'));
-    gulp.src('./index.html').pipe(gulp.dest('./online/'));
+    gulp.src('./*.html').pipe(gulp.dest('./online/'));
     gulp.src('./lib/*').pipe(gulp.dest('./online/lib/'));
 })
 
 gulp.task('default', function() {
-    gulp.run('lint', 'scripts', 'less', 'min-styles','online');
+    gulp.run('lint', 'scripts', 'less', 'min-styles', 'online');
     gulp.watch('./js/*.js', function() {
         gulp.run('lint', 'scripts');
     });
     gulp.watch('./less/*.less', function() {
         gulp.run('less', 'min-styles');
     });
-    gulp.watch(['./index.html','./images/*'],function(){
+    gulp.watch(['./index.html', './images/*'], function() {
         gulp.run('online');
-    })
+    });
 })
